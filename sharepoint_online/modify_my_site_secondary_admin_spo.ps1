713bac3a40cfe3f94ea19abc7c0de35d2581302d
adilyoosuf <adilyoosuf@gmail.com>
2021-07-15 13:46:32 +0530

    Add files via upload

---
+++ sharepoint_online/modify_my_site_secondary_admin_spo.ps1
@@ -0,0 +1,23 @@
+﻿#this powershell script is used to modify the my site secondary admin privilege of a user in sharepoint online(spo)
+
+#spo admin credentials
+#define the spo admin url and the spo admin e-mail below
+$SpoAdminUrl=""
+$SpoAdmin=""
+
+#connect to sharepoint online management shell
+Connect-SPOService -Url $SpoAdminUrl -Credential $SpoAdmin
+
+#get sharepoint(onedrive) sites of individual users and assign it to the variable OneDriveSites
+$OneDriveSites=Get-SPOSite -IncludePersonalSite $true -Limit ALL -Filter {Url -like "-my.sharepoint.com/personal"}
+
+#for loop to modify spo my site secondary admin privilege of a user
+
+foreach($OneDriveSite in $OneDriveSites)
+
+{
+Set-SPOUser -Site $OneDriveSite.Url -LoginName $SpoAdmin -IsSiteCollectionAdmin $false
+
+#display result in console
+Write-Host "$SpoAdmin removed from OneDrive of "$OneDriveSite.Owner""
+}
